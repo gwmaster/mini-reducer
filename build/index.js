9c6b2a95b29448b49cd7ef50e23ac97673ed77d8
jenia <gwmaster.gwm@gmail.com>
2019-12-25 20:48:31 +0200

    React basic test component

--- build/index.js
+++
@@ -1,6 +0,0 @@
-import Card from './components/Card';
-import Box from './components/Box';
-module.exports = {
-  Card,
-  Box
-};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Card from './components/Card';
-import Box from './components/Box'
-module.exports = {
-    Card,
-    Box
-}
+import Card from "./components/Card";
+import Box from "./components/Box"
+export{
+  Card,
+  Box
+};

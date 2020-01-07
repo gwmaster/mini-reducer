b6c26a2e7ab8981e505c893624372b585d8cfc90
jenia <gwmaster.gwm@gmail.com>
2020-01-07 23:31:27 +0200

    init

--- build/index.js
+++
@@ -1,3 +0,0 @@
-import Card from "./components/Card";
-import Box from "./components/Box";
-export { Card, Box };
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Card from "./components/Card";
-import Box from "./components/Box"
-export{
-  Card,
-  Box
-};
